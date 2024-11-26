fd89b462a8f6dd8e1519b36d915b49376565d655
raymund_dev <raymundhermoso.dev@gmail.com>
2024-11-26 21:35:23 +0800

    FIX: pdf Link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Social from "./Social"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faDownload } from "@fortawesome/free-solid-svg-icons"
 
-import pdf from "../assets/files/RaymundHermoso.pdf"
+import pdf from "../assets/files/RAYMUND HERMOSO - CV.pdf"
 
 import profile from '../assets/profile.png'
 
